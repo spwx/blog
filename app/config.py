03642c46ca8c1da0422a1c0630f9df1638d63dc4
Sean Wall <sean@wall.ninja>
2017-07-15 21:32:59 -0400

    Define the database string in an env variable

--- app/config.py
+++ app/config.py
@@ -5,9 +5,9 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 mode = os.environ.get('MODE')
 
 if mode == 'dev':
-    SQLALCHEMY_DATABASE_URI = 'postgresql://blogdb:blogdb123@blogdb.cqxklygxzkdp.us-east-1.rds.amazonaws.com:5432/blogdb'
     FLASKS3_BUCKET_NAME = 'dev-cdn.wall.ninja'
     FLASKS3_FORCE_MIMETYPE = True
     FLASKS3_URL_STYLE = 'path'
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DBSTRING')
+
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DBSTRING')
